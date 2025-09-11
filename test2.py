452fbfc1feabe38e6f4d0063c8c0935edc32fbcf
SIMRANRAINA2023a1r065 <146851787+SIMRANRAINA2023a1r065@users.noreply.github.com>
2025-09-11 12:17:15 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,31 @@
+##num1=eval(input("Enter First number:"))
+##num2=eval(input("Enter Second number:"))
+##if num1%2==0 and num2%2==0:
+##    res=num1+num2
+##    print("res=",res)
+##else:
+##    print("only even nums are allowed")
+    
+##per = int(input("Enter percentage:"))
+##if(per>=90 and per<=100):
+##    print("A")
+##elif(per>=80 and per<=90):
+##    print("B")
+##elif(per>=70 and per<=80):
+##    print("C")
+##else:
+##    print("No Grade")
+    
+##num=int(input("Enter number:"))
+##if(num>0):
+##    print("Positive")
+##else:
+##    print("Negative")
+##else if ladder
+per=int(input("Enter per:"))
+if(per>=60):
+    print("First division")
+    if(per>=75):
+        print("First distinction")
+else:
+    print("pass")
