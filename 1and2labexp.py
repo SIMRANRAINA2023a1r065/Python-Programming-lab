18df0c848d7664e7597e02bfb83c107daf490dff
SIMRANRAINA2023a1r065 <146851787+SIMRANRAINA2023a1r065@users.noreply.github.com>
2025-09-23 16:06:32 +0530

    Add files via upload

---
+++ 1and2labexp.py
@@ -0,0 +1,78 @@
+'''Activity -1:- To demonstrates type checking of various data types and the use of the following built-in functions:
+
+abs(),len(),min(),round(),isalnum(),type()'''
+
+
+"""# Define variables of different data types
+integer_num = -25
+float_num = 7.895
+string_text = "Hello123"
+list_data = [10, 20, 30, 5, 15]
+mixed_string = "Welcome!"
+alphanumeric_string = "Test123"
+
+# 1. type(): Returns the type of a variable.
+#Examples:
+print("1. type() function:")
+print("Type of integer_num:", type(integer_num))       
+print("Type of float_num:", type(float_num))           
+print("Type of string_text:", type(string_text))       
+print("Type of list_data:", type(list_data))       
+print()
+
+# 2. abs(): Returns the absolute (non-negative) value of a number.
+#Examples
+print("2. abs() function:")
+print("Absolute value of", integer_num, "is", abs(integer_num))  """
+print()
+
+"""# 3. len(): Returns the number of items in an object (string, list, etc.)
+#Examples
+print("3. len() function:")
+print("Length of string_text is", len(string_text))     
+print("Length of list_data is", len(list_data))         
+print()
+
+# 4. min(): Returns the smallest item in an iterable or between arguments.
+#Examples
+print("4. min() function:")
+print("Minimum in list_data is", min(list_data))        
+print()
+
+# 5. round(): Rounds a number to the nearest integer or specified decimal places.
+#Examples
+print("5. round() function:")
+print("Rounded value of", float_num, "is", round(float_num))      
+print("Rounded value to 2 decimals:", round(float_num, 2))         
+print()
+
+# 6. isalnum(): Returns True if all characters in the string are alphanumeric (letters or digits).
+#Examples
+print("6. isalnum() function:")
+print("Is string_text alphanumeric?", string_text.isalnum())       
+print("Is mixed_string alphanumeric?", mixed_string.isalnum())     
+print("Is alphanumeric_string alphanumeric?", alphanumeric_string.isalnum())"""
+
+'''-------------------------------------------------------------------------------------------------------------------------------------------------
+
+Activity-8:-Python program that converts temperature to and from Celsius and Fahrenheit. The user can choose the direction of conversion. '''
+
+
+print("Temperature Conversion Program")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = int(input("Enter your choice (1 or 2): "))
+
+if choice == 1:
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit =  (celsius * 9/5) + 32
+    print(str(celsius)," C is equal to ",str(fahrenheit)," F")
+
+elif choice == 2:
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print(str(fahrenheit)," F is equal to ",str(celsius),"°C")
+
+else:
+    print("Invalid choice. Please enter 1 or 2.")
