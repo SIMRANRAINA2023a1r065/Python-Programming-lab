8c08bb95ad4a9a3d0a97d1217adf7efebcec8011
SIMRANRAINA2023a1r065 <146851787+SIMRANRAINA2023a1r065@users.noreply.github.com>
2025-10-10 20:19:42 +0530

    Add files via upload

---
+++ 10october.py
@@ -0,0 +1,14 @@
+"""
+Function Demo
+Dev by KSG
+"""
+
+def add(num1:int,num2:int) ->int:
+#     add 2 numbers and 
+#    return an integer   #--> string object comment (#--> is a in-line comment)
+   return num1+num2 # return sum to it`s calling env`
+#pydentic is a module in python where type hinting is mandatory
+
+
+
+print(help(add))
